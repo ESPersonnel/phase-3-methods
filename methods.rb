f107da118dde772bc6c7e663d9ebfae50dd8f3b1
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-08-16 08:36:48 +0300

    commit tree a1663f6b8c21f2fab6170f6419dc07945cb8e0de

--- methods.rb
+++ methods.rb
@@ -1,1 +1,32 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+# def greet(name)
+#     "Hello, #{name}!"
+#     "Hello, #{name}!"
+# end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add (a, b)
+    a + b
+end
+
+    
+# Define a method #halve that takes one number as an argument and returns the that number's value, divided by two. If the argument is not an integer, it should return nil and not throw an error.
+def halve (a)
+    if a % 2 == 0
+        a / 2
+    end
+end
+
