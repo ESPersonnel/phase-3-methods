afca0731ad9552f113e27ed587a261f02f012dc6
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-08-16 08:37:58 +0300

    Done.

--- methods.rb
+++ methods.rb
@@ -1,1 +1,34 @@
 # Your code here!
+
+# Code 1
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+# def greet(name)
+#     "Hello, #{name}!"
+#     "Hello, #{name}!"
+# end
+
+# Code 2
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+# Code 3
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+# Code 4
+def add (a, b)
+    a + b
+end
+
+ # Code 5   
+def halve (a)
+    if a % 2 == 0
+        a / 2
+    end
+end
+
